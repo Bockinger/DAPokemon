07dd623ae63fecfd4b2fb1458addabe5221184ce
Bockinger <44947911+Bockinger@users.noreply.github.com>
2025-01-19 13:23:31 +0100

    Search OK

--- main.js
+++ main.js
@@ -4,7 +4,8 @@ let pokeFirstURL = "https://pokeapi.co/api/v2/pokemon/"
 
 let pokeArrayAll = [];
 let pokeArraysingle = [];
-
+let SeachArray = [];
+let serachID = [];
 let pokeArraySearch = [];
 
 let pokemonsLoad = 24;
@@ -59,12 +60,16 @@ function searchPokemon() {
   document.getElementById('btnnext').style.display = 'none';
   let resultPokeSearch = [];
   let pokeSearch = pokeArrayAll.results.filter(p => p.name.toLowerCase().startsWith(searchString.toLowerCase()));
-  console.log("Gefunden Arra", pokeSearch);
   pokeSearch.map(p => {
     let id = p.url.split("/").filter(part => part).pop();
     resultPokeSearch.push(id);
   });
+
+  serachID = resultPokeSearch;
+  console.log("Gefunden Arra", pokeSearch);
+  console.log("Result ", resultPokeSearch);
   document.getElementById('poketCard').innerHTML = "";
+
   loadPokeSelect(resultPokeSearch, true);
 }
 
@@ -86,27 +91,33 @@ function pokeNextSelection() {
 }
 
 
-async function loadPokeSelect(pokeSelec, search, searchIds) {
+async function loadPokeSelect(pokeSelec, search) {
   loader(true);
   try {
+
+    console.log("PoketSelect ==  ", pokeSelec);
+
     const pokets = pokeSelec.map(id => `${pokeFirstURL}${id}`);
     let fetchPromis = pokets.map(singleURL => fetch(singleURL).then(Poketresponse => {
       return Poketresponse.json();
     }));
     let ArrayRead = await Promise.all(fetchPromis)
     if (search) {
-      renderPokeCard(ArrayRead);
-      pokeArraySearch = ArrayRead;
       pokeSearchMode = true;
+      SeachArray = ArrayRead;
+      //console.log("Search Ids ", ResultpokeSearch);
+
+      console.log("Such ARRAY ", SeachArray);
+
+      renderPokeCard(ArrayRead); //serach Id
     } else {
       pokeSearchMode = false;
       let oldArray = pokeArraysingle
       pokeArraysingle = oldArray.concat(ArrayRead);
       renderPokeCard(pokeArraysingle);
-
     }
   } catch {
-    console.log("Fehler beim abrufen der Daten");
+    console.log("Fehler beim abrufen der Daten !!!");
   }
 }
 
@@ -114,13 +125,12 @@ async function loadPokeSelect(pokeSelec, search, searchIds) {
 function renderPokeCard(poketList) {
   let i = -1;
   let element = document.getElementById('poketCard');
-  let pokemonDetail = poketList.map(pokemon => {
-    i = i + 1
-    console.log(i);
+  poketList.map(pokemon => {
+    i++;
     let img = pokeArraysingle[i].sprites.other.home.front_default;
     let pokType = typeCheck(pokemon);
     let colorCard = colorCardCheck(pokType[0]);
-    element.innerHTML += templatePokeCard(pokemon, img, pokType, colorCard)
+    element.innerHTML += templatePokeCard(pokemon, img, pokType, colorCard, i);
   })
   document.getElementById('btnnext').classList.remove('d-none');
   loader(false);
@@ -144,10 +154,17 @@ function colorCardCheck(type) {
 }
 
 
-function templatePokeCard(Poklist, img, typ, color) {
+function templatePokeCard(Poklist, img, typ, color, index) {
+  console.log("Index bei Template ", index);
+
+  if (pokeSearchMode) {
+    id = index + 1;
+  } else {
+    id = Poklist.id;
+  }
   return `
   <div  class="col">
-    <div id="pCard" class="pCard" style="background-color: ${color};" onclick="pokeInfoDetail(${Poklist.id})">
+    <div id="pCard" class="pCard" style="background-color: ${color};" onclick="pokeInfoDetail(${id})">
       <div class="pCard_body"></div>
       <p class="pCard_name">${Poklist.name}</p>
       <img id="pCard_img" class="pCard_img" src="${img}">
@@ -162,13 +179,6 @@ function templatePokeCard(Poklist, img, typ, color) {
 }
 
 function pokeInfoDetail(id) {
-
-  if (pokeSearchMode) {
-    console.log("Diese Karte kann nicht geladen werden SearchMode");
-    // return
-  }
-
-
   console.log("Card mit Nummer ", id);
   noneOrflexDisplay([{ "lockoutDisplay": "flex" }, { "detailCard": "flex" }])
   document.getElementById('body').style.overflow = 'hidden';
@@ -213,18 +223,26 @@ function renderPokemonDetailCard(id) {
 
 
 function templatePokemonDetailCard(id) {
+  console.log("Detail Caed Id ", id);
   id = id - 1
+  let pokeArray = [];
 
-  console.log("Alle Array == ", pokeArraysingle);
-  console.log("Alle Array selection == ", pokeSelection);
-  console.log("Search Array ==", pokeArraySearch)
-  console.log("NAme über hundert ", pokeArraySearch[1].name);
+
+  if (pokeSearchMode) {
+    pokeArray = SeachArray;
+    console.log(SeachArray);
+
+
+  } else {
+    pokeArray = pokeArraysingle
+  }
+
 
 
   return `
   <div class="detail_Card_Header">
-        <spam>${pokeArraysingle[id].name}</spam>
-        <img class="img_Pokemon" src="${pokeArraysingle[id].sprites.other.home.front_default}">
+        <spam>${pokeArray[id].name}</spam>
+        <img class="img_Pokemon" src="${pokeArray[id].sprites.other.home.front_default}">
         <img onclick="closeDetailCard()" class="close_Button" src="./assets/close.svg">
       </div>
       <hr>
@@ -241,29 +259,29 @@ function templatePokemonDetailCard(id) {
               <tbody>
                 <tr>
                   <td>hp</td>
-                  <td>${pokeArraysingle[id].stats[0].base_stat}</td>
+                  <td>${pokeArray[id].stats[0].base_stat}</td>
                 </tr>
                 <tr>
                   <td>attack</td>
-                  <td>${pokeArraysingle[id].stats[1].base_stat}</td>
+                  <td>${pokeArray[id].stats[1].base_stat}</td>
                 </tr>
                  <tr>
                   <td>defense</td>
-                  <td>${pokeArraysingle[id].stats[2].base_stat}</td>
+                  <td>${pokeArray[id].stats[2].base_stat}</td>
                 </tr>
 
                  <tr>
                   <td>special-attack</td>
-                  <td>${pokeArraysingle[id].stats[3].base_stat}</td>
+                  <td>${pokeArray[id].stats[3].base_stat}</td>
                 </tr>
 
                  <tr>
                   <td>special-defense</td>
-                  <td>${pokeArraysingle[id].stats[4].base_stat}</td>
+                  <td>${pokeArray[id].stats[4].base_stat}</td>
                 </tr>
                  <tr>
                   <td>speed</td>
-                  <td>${pokeArraysingle[id].stats[5].base_stat}</td>
+                  <td>${pokeArray[id].stats[5].base_stat}</td>
                 </tr>
               </tbody>
             </table>
